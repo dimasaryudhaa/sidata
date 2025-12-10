f0c349d1879143f3da1a8906c8295ff60173e85c
dimasaryudhaa <dimasaryudha0309@gmail.com>
2025-12-10 08:43:54 +0700

    p

--- resources/views/data.blade.php
+++ resources/views/data.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>SiData - Dashboard</title>
+    <title>SiData</title>
 
     <link rel="stylesheet" href="assets/vendors/iconly/bold.css">
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
